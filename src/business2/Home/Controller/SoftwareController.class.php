510a0037f408a34ff5676d15d124a3c9409deb1f
lijianying10 <lijianying12@gmail.com>
2014-06-17 00:19:46 +0800

    update

--- src/business2/Home/Controller/SoftwareController.class.php
+++ src/business2/Home/Controller/SoftwareController.class.php
@@ -23,17 +23,46 @@ class SoftwareController extends Controller {
 
     public function show_form()
     {
-
-        $array['ak']    =    '47';
-        $array['ab']   =    '48';
-//
-//        $temppage = T('Public/test');
-//        $content = T('Public/test')->fetch($parameters);
-//        $this->fetch($parameters);
-////        $this->display($temppage);
-////        $this->show($content,'utf-8');
-//        var_dump($content);
-        $this->assign($array);
+        $index = I('get.index');
+        $RowIndex =  I('get.RowIndex');
+        $Data = M('software');
+        $map['s_id']=$RowIndex;
+        $map = $Data->where($map)->select();
+        $map[0]['index']=$index;
+        $this->assign($map[0]);
         $this->display();
+    }
+    public function save()
+    {
+        echo 'this is save';
+
+        var_dump(I('get.'));
+        echo '===================post=================';
+        var_dump(I('post.'));
+
+    }
+    public function update()
+    {
+        try{
+            $index = I('get.id');
+            $Data = M('software');
+            $map['s_id']=$index;
+            $map = $Data->where($map)->select();//query
+
+            //changevalue
+            $map[0]['s_Title']=I('post.s_Title');
+            $map[0]['s_language']=I('post.s_language');
+            $map[0]['s_offical_site']=I('post.s_offical_site');
+            $map[0]['s_plateform']=I('post.s_plateform');
+            $map[0]['s_URL_baidu']=I('post.s_URL_baidu');
+            $map[0]['s_URL_offical']=I('post.s_URL_offical');
+            $map[0]['s_Introduction']=I('post.s_Introduction');
+            $Data->save($map[0]);
+            echo '1';
+        }
+        catch(Exception $e)
+        {
+            echo '0';
+        }
     }
 }
